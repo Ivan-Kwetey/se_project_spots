c4198dc4755775eb2d1dc342c789dd87f8eeaa7f
ivankwetey <navitunes@gmail.com>
2025-04-28 18:50:46 -0700

    form submission

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
-
 const MODAL_OPEN_CLASS = "modal_is-opened";
+
 function openModal(modal) {
   modal.classList.add(MODAL_OPEN_CLASS);
 }
@@ -16,8 +16,43 @@ const newPostBtn = document.querySelector(".profile__post");
 const newPostModal = document.querySelector("#new-post-modal");
 const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
 
-editProfileBtn.addEventListener("click", () => openModal(editProfileModal));
+const profileNameElement = document.querySelector(".profile__name");
+const profileJobElement = document.querySelector(".profile__title");
+
+const profileFormElement = editProfileModal.querySelector(".modal__form");
+const nameInput = editProfileModal.querySelector("#profile-name-input");
+const jobInput = editProfileModal.querySelector("#profile-description-input");
+
+const addCardFormElement = newPostModal.querySelector(".modal__form");
+const imageLinkInput = newPostModal.querySelector("#image-link-input");
+const captionInput = newPostModal.querySelector("#post-caption-input");
+
+editProfileBtn.addEventListener("click", () => {
+  nameInput.value = profileNameElement.textContent;
+  jobInput.value = profileJobElement.textContent;
+  openModal(editProfileModal);
+});
+
 editProfileCloseBtn.addEventListener("click", () => closeModal(editProfileModal));
 
 newPostBtn.addEventListener("click", () => openModal(newPostModal));
+
 newPostCloseBtn.addEventListener("click", () => closeModal(newPostModal));
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+  profileNameElement.textContent = nameInput.value;
+  profileJobElement.textContent = jobInput.value;
+  closeModal(editProfileModal);
+}
+
+profileFormElement.addEventListener("submit", handleProfileFormSubmit);
+
+function handleAddCardSubmit(evt) {
+  evt.preventDefault();
+  console.log("Image Link:", imageLinkInput.value);
+  console.log("Caption:", captionInput.value);
+  closeModal(newPostModal);
+}
+
+addCardFormElement.addEventListener("submit", handleAddCardSubmit);
